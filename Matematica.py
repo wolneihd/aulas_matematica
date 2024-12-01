08ebb90b677eadc364661bf8e21b36ec643a536d
Wolnei Hellmann Dircksen <wolneihd@gmail.com>
2024-12-01 13:31:05 -0300

    codificando as aulas de matematica

---
+++ Matematica.py
@@ -0,0 +1,39 @@
+def mmc(denominadores: list[int], mostrar_conta: bool = False) -> float: 
+    """
+    Método que retorna o MMC - Mínimo Múltiplo Comum para uma lista de números inteiros.
+    <br>
+    `denominadores`: lista de números (obrigatório)
+    <br>
+    `mostrar_conta`: mostra o cálculo passo-a-passo - tipo: bool - valor default: False.
+    """
+    copia = denominadores.copy()
+    for denominador in denominadores:
+        
+        is_primo = True
+        array_primos = []
+
+        for index in range(2,11):
+            if (denominador%index) == 0:
+                is_primo = False
+        if is_primo:
+            array_primos.append(denominador)
+
+    divisores = []
+    for index in range(2,11):
+        for i, denominador in enumerate(denominadores):
+            if (denominador%index) == 0:                
+                denominadores[i] = denominador/index
+                divisores.append(index)       
+                if mostrar_conta:
+                    print(f'{copia} | {index}')  
+
+    resultado = 1
+    for numero in divisores:
+        resultado = resultado * numero
+
+    for numero in array_primos:
+        resultado = resultado * numero
+        if mostrar_conta:
+            print(f'{copia} | {numero}')  
+        
+    print(resultado)
